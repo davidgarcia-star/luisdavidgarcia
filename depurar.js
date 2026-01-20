5ce1e73f817e6dd077d4698fe7fbfe95e608164c
davidgarcia-star <luisdavidgarcia.oficial@gmail.com>
2026-01-19 23:58:12 -0400

    Add files via upload

---
+++ depurar.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  let respuesta = confirm("SI ERES UNA PERSONA, HAZ CLIC EN ACEPTAR. DE LO CONTRARIO, HAZ CLIC EN CANCELAR.");
+
+  if (respuesta) {
+    alert("¡BIENVENIDO/A A ESTA PÁGINA WEB!");
+     // Aquí la página sigue cargando normalmente
+    
+  } else {
+    alert("ACCESO DENEGADO. SÓLO LAS PERSONAS PUEDEN ACCEDER A ESTA PÁGINA WEB.");
+    window.location.href = "about:blank"; 
+    // Redirige a una página en blanco
+  }
+};
+
+document.getElementById("miAudio").play();
+
